828ff8b64c1b83d18d253f3125ae59378f00e4ea
vansen345 <86304461+vansen345@users.noreply.github.com>
2026-04-17 16:24:06 +0700

    -- update

--- my-app/src/config/baseQuery.ts
+++ my-app/src/config/baseQuery.ts
@@ -1,4 +1,5 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_CONFIG_CLIENT } from "../lib/config_api_client";
 
 // export const API_BASE_URL = "https://react-web-backend.onrender.com/api";
 
@@ -14,7 +15,8 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // });
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "/piepapi/home",
+  // baseUrl: "/piepapi/home",
+  baseUrl: `${API_CONFIG_CLIENT.BASE_URL.replace('/api', '')}/piepapi/home`,
   prepareHeaders: (headers) => {
     const token = "your_token_here";
     if (token) {
